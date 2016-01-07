df7eadeeea5e8f215050f9f59440fc4b50eac1ad
Deepa Kackar <dkackar99@yahoo.com>
2016-01-07 15:54:40 -0800

    Added tests to connect_four_spec

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -10,8 +10,20 @@ describe "ConnectFour" do
     it 'should give you a Board' do
       expect(game.board).to be_a(Board)
     end
-    it 'should give you a Board' do
-      expect(game.board).to be_a(Board)
+
+    it 'should initialize opponent to 2' do
+      expect(game.opponent).to eq("2")
     end
+
+    it 'should initialize current_player to true' do
+      expect(game.current_player).to eq(true)
+    end
+
+   it 'should have receive correct input value from user' do
+       inp = allow(game.print_intro).to receive(:gets).and_return("1,2")
+       if inp == "1"
+          expect(game.opponent).to be_a(Computer)
+       end
+   end
   end  
 end    
